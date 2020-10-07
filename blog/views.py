bfea50060eb8559095917968578c05b3fb8716e5
Akashkarmokar <akashcsemu@gmail.com>
2020-10-07 09:41:52 +0600

    init

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render,HttpResponseRedirect
-
+from .forms import SignUpForm,LoginForm,PostForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import Group
+from .models import Post
 # Create your views here.
 
 #home
 def home(request):
-    return render(request,'blog/home.html')
+    posts = Post.objects.all()
+    return render(request,'blog/home.html',{'posts':posts})
 
 #about
 def about(request):
@@ -16,16 +21,93 @@ def contact(request):
 
 #Dashboard
 def dashboard(request):
-    return render(request,'blog/dashboard.html')
+    if request.user.is_authenticated:
+        posts = Post.objects.all()
+        user = request.user
+        full_name = user.get_full_name()
+        gps = user.groups.all()
+        return render(request,'blog/dashboard.html',{'posts':posts,'full_name':full_name,'groups':gps})
+    else:
+        return HttpResponseRedirect('/user_login/')
 
 #Logout
 def user_logout(request):
+    logout(request)
     return HttpResponseRedirect('/')
 
 #Login
 def user_login(request):
-    return render(request,'blog/login.html')
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = LoginForm(request=request,data=request.POST)
+            if form.is_valid():
+                uname = form.cleaned_data['username']
+                upass = form.cleaned_data['password']
+                authenticate_user_obj = authenticate(username=uname,password=upass)
+                if authenticate_user_obj is not None:
+                    login(request,authenticate_user_obj)
+                    messages.success(request,'Logged in Successfully !!')
+                    return HttpResponseRedirect('/dashboard/')
+        else:
+            form = LoginForm()
+        return render(request,'blog/login.html',{'form':form})            
+    else:
+        return HttpResponseRedirect('/dashboard/')
 
 #signup
-def signup(request):
-    return render(request,'blog/signup.html')
+def user_signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            messages.success(request,'Congratulations You become an author')
+            user = form.save()
+            groups = Group.objects.get(name='Author')
+            user.groups.add(groups)
+    else:
+        form = SignUpForm()
+    return render(request,'blog/signup.html',{'form':form})
+
+
+
+#Add New Post
+def  add_post(request):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            form = PostForm(request.POST)
+            if form.is_valid():
+                title = form.cleaned_data['title']
+                description = form.cleaned_data['desc']
+                pst = Post(title=title,desc=description)
+                pst.save()
+                form = PostForm()
+        else:
+            form = PostForm()
+        return render(request,'blog/addpost.html',{'form':form})
+    else:
+        return HttpResponseRedirect('/user_login/')
+
+
+#Update Post
+def  update_post(request,id):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            pi = Post.objects.get(pk=id)
+            form = PostForm(request.POST,instance=pi)
+            if form.is_valid():
+                form.save()
+        else:
+            pi = Post.objects.get(pk=id)
+            form = PostForm(instance=pi)
+        return render(request,'blog/updatepost.html',{'form':form})
+    else:
+        return HttpResponseRedirect('/user_login/')
+
+#Delete Post
+def  delete_post(request,id):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            pi = Post.objects.get(pk=id)
+            pi.delete()
+        return HttpResponseRedirect('/dashboard/')
+    else:
+        return HttpResponseRedirect('/user_login/')
--- miniblog/urls.py
+++ miniblog/urls.py
@@ -23,7 +23,10 @@ urlpatterns = [
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
     path('dashboard/',views.dashboard,name='dashboard'),
-    path('user_login',views.user_login, name='user_login'),
-    path('signup',views.signup, name='signup'),
-    path('user_logout',views.user_logout, name='user_logout'),   
+    path('user_login/',views.user_login, name='user_login'),
+    path('signup',views.user_signup, name='signup'),
+    path('user_logout',views.user_logout, name='user_logout'),
+    path('addpost/',views.add_post,name='addpost'),
+    path('updatepost/<int:id>/',views.update_post,name='updatepost'),
+    path('delete/<int:id>/',views.delete_post,name='deletepost'),   
 ]
